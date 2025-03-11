0c068b07b47ced6ee888dc88363eca13f694acb3
Ruairí Glackin <ruairiglackin@gmail.com>
2025-03-11 01:05:32 +0000

    added error handling for invalid ticker

--- src/stock_ticker.py
+++ src/stock_ticker.py
@@ -6,12 +6,16 @@ logger = logging.getLogger(__name__)
 
 def get_ticker_price(ticker_name: str):
     # Get the ticker of the stock
-    ticker = yf.Ticker(ticker_name)
+    try:
+        
+        ticker = yf.Ticker(ticker_name)
     
-    # Get the current price of the stock
-    ticker_price = ticker.history(period="1d")["Close"].iloc[-1]
-    if ticker_price:
-        logger.info("Current price of %s is %s USD", ticker_name, ticker_price)
-        return ticker_price
-    logger.error("Failed to retrieve %s price data. May be rate limited or unavailable", ticker_name)
+        # Get the current price of the stock
+        ticker_price = ticker.history(period="1d")["Close"].iloc[-1]
+        if ticker_price:
+            logger.info("Current price of %s is %s USD", ticker_name, ticker_price)
+            return ticker_price
+        logger.error("Failed to retrieve %s price data. May be rate limited or unavailable", ticker_name)
+    except Exception as e:
+        logger.error("Failed to retrieve %s price data: %s", ticker_name, e)
     return None
